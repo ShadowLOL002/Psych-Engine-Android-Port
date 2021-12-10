9b175d9a3075db70791fcc364989d85f52db9e12
ShadowLOL002 <95757830+ShadowLOL002@users.noreply.github.com>
2021-12-10 08:28:51 -0300

    Add files via upload

--- source/DialogueBoxPsych.hx
+++ source/DialogueBoxPsych.hx
@@ -177,18 +177,18 @@ class DialogueBoxPsych extends FlxSpriteGroup
 		this.dialogueList = dialogueList;
 		spawnCharacters();
 
-		box = new FlxSprite(70, 370);
-		box.frames = Paths.getSparrowAtlas('speech_bubble');
+		box = new FlxSprite(46, 456);
+		box.frames = Paths.getSparrowAtlas('sus_speechSquares');
 		box.scrollFactor.set();
 		box.antialiasing = ClientPrefs.globalAntialiasing;
-		box.animation.addByPrefix('normal', 'speech bubble normal', 24);
-		box.animation.addByPrefix('normalOpen', 'Speech Bubble Normal Open', 24, false);
-		box.animation.addByPrefix('angry', 'AHH speech bubble', 24);
-		box.animation.addByPrefix('angryOpen', 'speech bubble loud open', 24, false);
-		box.animation.addByPrefix('center-normal', 'speech bubble middle', 24);
-		box.animation.addByPrefix('center-normalOpen', 'Speech Bubble Middle Open', 24, false);
-		box.animation.addByPrefix('center-angry', 'AHH Speech Bubble middle', 24);
-		box.animation.addByPrefix('center-angryOpen', 'speech bubble Middle loud open', 24, false);
+		box.animation.addByPrefix('normal', 'speech square normal', 24);
+		box.animation.addByPrefix('normalOpen', 'speech square open', 24, false);
+		box.animation.addByPrefix('angry', 'speech square normal', 24);
+		box.animation.addByPrefix('angryOpen', 'speech square open', 24, false);
+		box.animation.addByPrefix('center-normal', 'speech square normal', 24);
+		box.animation.addByPrefix('center-normalOpen', 'speech square open', 24, false);
+		box.animation.addByPrefix('center-angry', 'speech square normal', 24);
+		box.animation.addByPrefix('center-angryOpen', 'speech square open', 24, false);
 		box.animation.play('normal', true);
 		box.visible = false;
 		box.setGraphicSize(Std.int(box.width * 0.9));
